d7dfe9920097d59eaba6b45e7b7ecc9c98922a5e
ganadhish1999 <acharekarganadhish1999@gmail.com>
2020-03-25 23:42:07 +0530

    changes in karatsuba.py

--- divide and conquer/karatsuba.py
+++ divide and conquer/karatsuba.py
@@ -1,24 +1,43 @@
-# cook your dish here
-from math import ceil, floor;
-def karatsuba(x, y):
+from math import floor
+def add_zero(numberString, zeros, left = True):
+
+    """Return the string with zeros added to the left or right."""
+    for i in range(zeros):
+        if left:
+            numberString = '0' + numberString
+        else:
+            numberString = numberString + '0'
+    return numberString
     
-    if x<10 and y<10:
-        return x*y
+def karatsuba(x ,y):
+    x = str(x)
+    y = str(y)
+    #base case 
+    if len(x) == 1 and len(y) == 1:
+        return int(x) * int(y)
+    
         
-    m = max(len(str(x)), len(str(y)))
-    n = ceil(m/2)
-
-    x_h = floor(x/(10**n))
-    x_l = x%(10**n)
+    if len(x) < len(y):
+        x = add_zero(x, len(y) - len(x))
+    elif len(y) < len(x):
+        y = add_zero(y, len(x) - len(y))
+    n = len(x)
+    j = floor(n/2)
     
-    y_h = floor(y/(10**n))
-    y_l = y%(10**n)
+    b_add_zero = n - j
+    a_add_zero = b_add_zero * 2
+    xl= int(x[:j])
+    xr = int(x[j:])
+    yl = int(y[:j])
+    yr = int(y[j:])
+    #recursively call
+    a = karatsuba(xl, yl)
+    B = karatsuba(xr, yr)
+    k = karatsuba(xl + xr, yl + yr)
+    A = int(add_zero(str(a), a_add_zero, False))
+    C = int(add_zero(str(k - a - B), b_add_zero, False))
 
-    a = karatsuba(x_h, y_h)
-    b = karatsuba(x_l, y_l)
-    c = karatsuba((x_h + x_l), (y_h + y_l)) - a - b
-
-    return int(a*(10**(n*2)) + c*(10**n) + b)
+    return A + B + C
     
 x = int ( input("Enter first number") )
 y = int ( input("Enter second number") )
